90a41295791abb893011da9b4e3725bc1633ca47
Sanskar singh <rsanskar654@gmail.com>
2025-08-11 14:02:55 +0530

    dc

--- Server/routes/urlRoute.js
+++ Server/routes/urlRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getUrls, handleGenerateshorturl } from "../controler/urlcontroller.js";
-import { auth } from "../middleware/auth.js";
+
 import { verifyUser } from "../middleware/verifyuser.js";
 
 const router = express.Router();
